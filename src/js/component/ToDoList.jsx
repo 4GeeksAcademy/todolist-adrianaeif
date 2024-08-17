202238175fcf60e2102028a788171c408098a9a2
adrianaeif <adrianaisea02@gmail.com>
2024-08-17 02:11:59 +0000

    función delete

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -8,13 +8,29 @@ const ToDoList= () => {
 
         const [taskList, setTaskList] = useState([]);
 
+        // cargar tarea
         const loadTask = async() => {
-            const urlApi = 'https://playground.4geeks.com/todo/users/adrianaeif'
-            const response = await fetch(urlApi);
-            const data = await response.json();
-            setTaskList(data.todos)
+            try {
+                const urlApi = 'https://playground.4geeks.com/todo/users/adrianaeif'
+                const response = await fetch(urlApi);
+                const data = await response.json();
+                setTaskList(data.todos)
+
+            } catch (error) {
+                console.log(error)
+            }
         }
 
+        //crear usuario
+
+        const createUser = async () => {
+            const response = await fetch ('https://playground.4geeks.com/todo/users/adrianaeif', {
+                method: 'POST',
+            })
+        }
+
+
+        // crear tarea
         const createTask = async () =>{
             const response = await fetch ('https://playground.4geeks.com/todo/todos/adrianaeif', {
                 method: 'POST',
@@ -27,20 +43,28 @@ const ToDoList= () => {
                 }
             });
             const data = await response.JSON();
-
+            
         }
-    
-        const deleteTask = async (newTask) =>{
+        
+        // borrar una tarea
+        const deleteTask = async (todoId, indice) =>{
             const id = newTask.id;
-            const response = await fetch ('https://playground.4geeks.com/todo/todos/${id}', {
+            const response = await fetch ('https://playground.4geeks.com/todo/todos/' + todoId, {
                 method: 'DELETE',
-               });
-               const data = await response.JSON();
+            });
+            if(response.ok){
+                setTaskList(taskList
+                    .filter((_tarea, indiceABorrar)=>{
+                        return indice != indiceABorrar
+                }))
+
+            }
             }
 
         
 
         useEffect( () => {
+            createUser();
             loadTask();
         },[]);
 
@@ -49,7 +73,7 @@ const ToDoList= () => {
 
 
             {/* Barra para agregar tarea */}
-            <div class="input-group mt-5 p-4 centered-container">
+            <div className="input-group mt-5 p-4 centered-container">
             <input type="text" className="form-control"  value={newTask}  placeholder="What do you want to do next?"
 				onChange={(event) => setNewTask(event.target.value)}
                 
@@ -67,24 +91,21 @@ const ToDoList= () => {
                 </div>
 
                {/* Alerta de tareas sin agregar */}
-              {(taskList.length == 0) && <div class="text-center p-3 mb-4 fs-5">No tasks, add a task</div>}
+              {(taskList.length == 0) && <div className="text-center p-3 mb-4 fs-5">No tasks, add a task</div>}
 
-              {/* Borrar tarea REVISAR */}
+              {/* Borrar tarea */}
               {taskList.map((tarea, indice)=> 
               <Task task={tarea}  key={indice} onRemove={()=>{
-                setTaskList(taskList
-                    .filter((_tarea, indiceABorrar)=>{
-                        return indice != indiceABorrar
-                }))
+                deleteTask(tarea.id, indice);
               }}/> )}
 
                {/* pie de página */}
-              <div class="card text-center"></div>
-              <div class="text-muted">{taskList.length} items left</div>
+              <div className="card text-center"></div>
+              <div className="text-muted">{taskList.length} items left</div>
 
               {/* Eliminar todas las tareas REVISAR*/}
             <div>
-                <button className="btn btn-danger m-5">
+                <button className="btn btn-danger m-5" onClick={() => {}}>
                     Eliminar todas las tareas
                 </button>
             </div>
